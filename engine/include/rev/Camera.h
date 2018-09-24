c7d72f55d63436474ebb4565c090403f84df0c1b
Jackson Gardner <eyebrowsoffire@gmail.com>
2018-09-23 17:04:25 -0700

    Camera.

---
+++ engine/include/rev/Camera.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace rev
+{
+
+class Camera
+{
+  public:
+    Camera();
+
+    void setPosition(const glm::vec3 &position);
+    void setTarget(const glm::vec3 &target);
+    void setUpVector(const glm::vec3 &target);
+
+    void setNearClip(float near);
+    void setFarClip(float far);
+    void setFieldOfView(float fov);
+    void setAspectRatio(float aspect);
+
+    const glm::mat4 &getViewMatrix();
+    const glm::mat4 &getProjectionMatrix();
+
+  private:
+    void refreshViewMatrix();
+    void refreshProjectionMatrix();
+
+    glm::vec3 _position;
+    glm::vec3 _target;
+    glm::vec3 _upVector;
+
+    glm::mat4 _viewMatrix;
+    glm::mat4 _projectionMatrix;
+
+    float _nearClip;
+    float _farClip;
+    float _fov;
+    float _aspect;
+    bool _viewDirty;
+    bool _projectionDirty;
+};
+
+} // namespace rev
---
+++ engine/src/Camera.cpp
@@ -0,0 +1,87 @@
+#include "rev/Camera.h"
+
+#include <glm/ext.hpp>
+
+namespace rev
+{
+
+Camera::Camera()
+    : _position(0.0f), 
+      _target(0.0, 0.0, -1.0), 
+      _upVector(0.0, 1.0, 0.0),
+      _nearClip(0.1f), 
+      _farClip(100.0f), 
+      _fov(glm::half_pi<float>()),
+      _aspect(16.0 / 9.0),
+      _viewDirty(true),
+      _projectionDirty(true) {}
+
+void Camera::setPosition(const glm::vec3 &position)
+{
+    _position = position;
+    _viewDirty = true;
+}
+
+void Camera::setTarget(const glm::vec3 &target)
+{
+    _target = target;
+    _viewDirty = true;
+}
+
+void Camera::setUpVector(const glm::vec3 &upVector)
+{
+    _upVector = upVector;
+    _viewDirty = true;
+}
+
+void Camera::setNearClip(float near)
+{
+    _nearClip = near;
+    _projectionDirty = true;
+}
+
+void Camera::setFarClip(float far) { _farClip = far; }
+
+void Camera::setFieldOfView(float fov)
+{
+    _fov = fov;
+    _projectionDirty = true;
+}
+
+void Camera::setAspectRatio(float aspect)
+{
+    _aspect = aspect;
+    _projectionDirty = true;
+}
+
+const glm::mat4 &Camera::getViewMatrix()
+{
+    refreshViewMatrix();
+    return _viewMatrix;
+}
+
+const glm::mat4 &Camera::getProjectionMatrix()
+{
+    refreshProjectionMatrix();
+    return _projectionMatrix;
+}
+
+void Camera::refreshViewMatrix()
+{
+    if (_viewDirty)
+    {
+        _viewMatrix = glm::lookAt(_position, _target, _upVector);
+        _viewDirty = false;
+    }
+}
+
+void Camera::refreshProjectionMatrix()
+{
+    if (_projectionDirty)
+    {
+        _projectionMatrix = glm::perspective(_fov, _aspect, _nearClip, _farClip);
+        _projectionDirty = false;
+    }
+}
+
+} // namespace rev
