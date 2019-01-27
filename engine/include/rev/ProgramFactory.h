17e27846bda56c30dd9161f728178f63cf46fe2c
Jackson Gardner <eyebrowsoffire@gmail.com>
2019-01-27 15:39:02 -0800

    Added a few comments here and there.

--- engine/include/rev/ProgramFactory.h
+++ engine/include/rev/ProgramFactory.h
@@ -7,9 +7,34 @@
 
 namespace rev
 {
+
+// Programs conform to the following contract
+//
+// class Program {
+//   public:
+//     // Programs must have a constructor that takes a ProgramResource (compiled shader program)
+//     // The ProgramFactory will use this to construct the object.
+//     Program(ProgramResource programResource);
+//
+//     // Programs must declare a meta-structure called "Source" which provides the shader source
+//     // code. The ProgramFactory compiles the source code on behalf of the program.
+//     struct Source {
+//         // Returns the vertex shader source code.
+//         static std::string_view getVertexSource();
+//
+//         // Returns the fragment shader source code.
+//         static std::string_view getFragmentSource();
+//     };
+// }
+
+// ProgramFactory compiles and instantiates program objects from their source code, or returns an
+// existing program object if it is already in use. This allows decoupled systems that use the
+// same shader program to not have to compile the shader source multiple times.
 class ProgramFactory
 {
   public:
+    // Compiles and instantiates the specified program type, or returns an existing instance of the
+    // program if it has already been compiled.
     template <typename ProgramType>
     std::shared_ptr<ProgramType> getProgram()
     {
--- engine/include/rev/SceneObjectGroup.h
+++ engine/include/rev/SceneObjectGroup.h
@@ -5,6 +5,9 @@
 
 namespace rev
 {
+
+// A type-erased object group.
+// This is the interface used by the rendering engine to render all objects in the group.
 class ISceneObjectGroup
 {
 public:
@@ -12,23 +15,41 @@ public:
     virtual void render(Camera &camera) = 0;
 };
 
+// Models conform to the following contract:
+// class Model
+// {
+//   public:
+//     // Models must specify an object type that represents a single instance of this model.
+//     using SceneObjectType;
+//
+//     // Renders the group of objects from the viewpoint of the passed in camera.
+//     void render(Camera& camera, const std::vector<std::shared_ptr<SceneObjectType>>& objects);
+// };
+
+// Encapsulates a group of objects that all render with the same model.
 template <typename ModelType>
 class SceneObjectGroup : public ISceneObjectGroup
 {
 public:
     using SceneObjectType = typename ModelType::SceneObjectType;
 
+    // SceneObjectGroup is constructed from the arguments for its model. The model is created on
+    // construction from the arguments.
     template <typename... Args>
     SceneObjectGroup(Args&& ...args)
     : _model(std::forward<Args>(args)...)
     {
     }
 
+    // Renders all the objects in the group. Used by the rendering engine.
     void render(Camera &camera) override
     {
         _model.render(camera, _objects);
     }
 
+    // Creates a new object, adds it to the group, and returns it.
+    // TODO: Maybe this should take arguments, that it should forward to the constructor of the
+    // SceneObjectType?
     std::shared_ptr<SceneObjectType> addObject()
     {
         auto object = std::make_shared<SceneObjectType>();
@@ -36,6 +57,8 @@ public:
 
         return std::move(object);
     }
+
+    // TODO: write removeObject();
 
 private:
     ModelType _model;
